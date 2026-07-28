61902825246f680f0ca01e75f16badb5396d84e4
Maxim <raychukmaxim@gmail.com>
2026-07-28 11:39:31 +0300

    Thrid commit

--- public/webhook.php
+++ public/webhook.php
@@ -2,10 +2,5 @@
 
 declare(strict_types=1);
 
-use SpaceWeatherBot\Core\Application;
+require __DIR__ . '/index.php';
 
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-$app = new Application();
-
-$app->run();
